bf19fc47e31f5eb072c64317cdb9c6aeee0ef030
maulidacy <cahyamaulida011@gmail.com>
2025-04-17 12:34:43 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,11 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
 
 $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
 
-$routes->get('produk', 'ProdukControllers::index');
-$routes->get('keranjang', 'KeranjangControllers::index');
+$routes->get('produk', 'ProdukControllers::index', ['filter' => 'auth']);
+$routes->get('keranjang', 'KeranjangControllers::index', ['filter' => 'auth']);
