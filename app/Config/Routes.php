1c2f6593882cb48d831bf06baefc7ed04e0a8c2f
maulidacy <cahyamaulida011@gmail.com>
2025-05-24 12:49:34 +0700

    Create, Update, dan Delete Data

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,6 +11,10 @@ $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
 
-$routes->get('produk', 'ProdukControllers::index', ['filter' => 'auth']);
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->get('produk/create', 'ProdukController::create', ['filter' => 'auth']);
+$routes->post('produk', 'ProdukController::create', ['filter' => 'auth']);
+$routes->post('produk/edit/(:any)', 'ProdukController::edit/$1', ['filter' => 'auth']);
+$routes->post('produk/delete/(:any)', 'ProdukController::delete/$1', ['filter' => 'auth']);
 $routes->get('keranjang', 'KeranjangControllers::index', ['filter' => 'auth']);
 $routes->get('profile', 'Home::profile', ['filter' => 'auth']);
